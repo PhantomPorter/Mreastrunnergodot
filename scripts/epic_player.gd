689ee7ad6da0e5afb8522e13a222f504ed279a03
PhantomPorter <ultramariosuper64@gmail.com>
2026-05-11 21:16:18 -0400

    idk brop

--- scripts/epic_player.gd
+++ scripts/epic_player.gd
@@ -19,3 +19,7 @@ func _physics_process(delta: float) -> void:
 	velocity.x = SPEED
 
 	move_and_slide()
+	for i in get_slide_collision_count():
+		var collision = get_slide_collision(i)
+		if (collision.get_collider().name == "spike"):
+			get_tree().reload_current_scene()
