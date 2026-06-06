16700f0f47b48f5f1b491d659a44c5d767bb147d
PhantomPorter <ultramariosuper64@gmail.com>
2026-06-05 21:57:49 -0400

    Added parrying(doesnt do anything yet)

--- scripts/epic_player.gd
+++ scripts/epic_player.gd
@@ -6,8 +6,16 @@ const JUMP_VELOCITY = -300.0
 @export var normal_gravity_scale: float = 1.0
 @export var glide_gravity_scale: float = 0.25 
 
-@onready var collision_shape: CollisionShape2D = $CollisionShape2D
+@onready var collision_shape: CollisionShape2D = $theguy
 @onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
+
+
+@onready var arm: Node2D = $arm
+var is_parrying: bool = false
+var is_on_cooldown: bool = false
+
+const PARRY_WINDOW: float = 0.67
+const COOLDOWN_DURATION: float = 0.50 
 
 # Hitbox tracking
 @onready var normal_shape: Shape2D = collision_shape.shape
@@ -48,7 +56,7 @@ func _physics_process(delta: float) -> void:
 
 	# Track if the player wants to slide right now
 	var is_sliding: bool = Input.is_action_pressed("ui_down")
-
+	
 	# --- HANDLE AIR STATE ---
 	if not is_on_floor():
 		if Input.is_action_pressed("ui_accept") and velocity.y > 0:
@@ -68,7 +76,6 @@ func _physics_process(delta: float) -> void:
 				sprite.play("default")
 				set_slide_state(false)
 	
-	# --- HANDLE GROUND STATE ---
 	else:
 		if Input.is_action_just_pressed("ui_accept"):
 			velocity.y = JUMP_VELOCITY
@@ -86,6 +93,9 @@ func _physics_process(delta: float) -> void:
 			set_slide_state(false)
 
 	move_and_slide()
+	if Input.is_action_just_pressed("parry"):
+		try_parry()
+	
 	
 	for i in get_slide_collision_count():
 		var collision = get_slide_collision(i)
@@ -93,7 +103,6 @@ func _physics_process(delta: float) -> void:
 		if collider and collider.is_in_group("spike"):
 			get_tree().call_deferred("reload_current_scene")
 
-# Helper function to cleanly swap hitboxes AND sprite positions together
 func set_slide_state(sliding: bool) -> void:
 	if sliding:
 		sprite.play("slide")
@@ -104,3 +113,43 @@ func set_slide_state(sliding: bool) -> void:
 		collision_shape.shape = normal_shape
 		collision_shape.position = normal_offset
 		sprite.position = normal_sprite_offset 
+
+func try_parry() -> void:
+	if is_parrying or is_on_cooldown:
+		return
+		
+	trigger_parry_sequence()
+
+func trigger_parry_sequence() -> void:
+	is_parrying = true
+	arm.visible = true
+	play_arm_sound(false)
+	
+	await get_tree().create_timer(PARRY_WINDOW).timeout
+	
+	is_parrying = false
+	arm.visible = false 
+	is_on_cooldown = true
+	
+	await get_tree().create_timer(COOLDOWN_DURATION).timeout
+	
+	is_on_cooldown = false
+
+func play_arm_sound(soundbyte: bool) -> void: 
+	if soundbyte: 
+		var sound = load("res://assest/parry.mp3") 
+		$arm/armsounds.stream = sound 
+		$arm/armsounds.play() 
+	else: 
+		var sound = load("res://assest/swoosh.ogg") 
+		$arm/armsounds.stream = sound 
+		$arm/armsounds.play()
+
+func _on_parry_box_area_entered(incoming_area: Area2D) -> void:
+	if incoming_area.is_in_group("enemy_attack"):
+		
+		if is_parrying:
+			play_arm_sound(true) 
+			
+			if incoming_area.has_method("reflect_or_recoil"):
+				incoming_area.reflect_or_recoil()
